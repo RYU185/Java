4350f303eea6a087529bc68bd7da98a3ab7dd125
RYU185 <fence365@naver.com>
2024-10-31 16:27:22 +0900

    Update Example10.java

--- ArrayString/src/Example10.java
+++ ArrayString/src/Example10.java
@@ -14,13 +14,14 @@ public class Example10 {
     public static void calculate2DArray(int[][] arr) {
         int total = 0;
         float average = 0;
-        for (int i=0; i<=arr.length; i++){
-            for (int j=0; j<=arr.length; j++) {
+        float num = 0;
+        for (int i=0; i<arr.length; i++){  // i열의 갯수 = 4개
+            for (int j=0; j < arr[i].length; j++) { // i 배열 안의 숫자 갯수 = 5개 )
                 total = total + arr[i][j];
+                num = num + 1;
             }
-            average = total / arr.length;
-
         }
+        average = total / num;
         System.out.println("total="+total); // 325
         System.out.println("average="+average); // 16.25
     }
