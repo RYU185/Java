96b687412ff48daa04860e74f7686bc1e930d650
RYU185 <fence365@naver.com>
2024-11-07 00:26:57 +0900

    Update Overriding_1.java

--- Inheritance/src/Overriding/Overriding_1.java
+++ Inheritance/src/Overriding/Overriding_1.java
@@ -26,7 +26,7 @@ public class Overriding_1 {
         value3.print(); // B 클래스10
         // A 클래스의 print()가 자식클래스의 print()로 오버라이딩되었기 때문
         // 교재 328페이지 필기 참고
-        // 오버라이딩은 메서드에만 적욛됨 (필드영역은 정적바인딩이기때문에 해당되지않는다)
+        // 오버라이딩은 메서드에만 적용됨 (필드영역은 정적바인딩이기때문에 해당되지않는다)
         System.out.println(value3.a); // 5
         B value4 = (B)value3; // 다운캐스팅
         System.out.println(value4.a); // 10
