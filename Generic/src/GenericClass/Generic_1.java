9722144ae4dd1118ee7648b076b24902df6c2a22
RYU185 <fence365@naver.com>
2024-11-15 11:12:54 +0900

    11/15 generic

--- Generic/src/GenericClass/Generic_1.java
+++ Generic/src/GenericClass/Generic_1.java
@@ -1,4 +1,43 @@
 package GenericClass;
 
+// 어떤 상황에 쓰이는가?
+class Apple {}
+class Goods1 {
+    private Apple apple = new Apple();
+
+    public Apple getApple() {
+        return apple;
+    }
+    public void setApple(Apple apple) {
+        this.apple = apple;
+    }
+}
+
+class Pencil{}
+class Goods2{
+    private Pencil pencil = new Pencil();
+
+    public Pencil getPencil() {
+        return pencil;
+    }
+    public void setPencil(Pencil pencil) {
+        this.pencil = pencil;
+    }
+}
+
 public class Generic_1 {
+    public static void main(String[] args) {
+        // 사과
+        Goods1 goods1 = new Goods1();
+        goods1.setApple(new Apple());
+        Apple apple = goods1.getApple();
+
+        Goods2 goods2 = new Goods2();
+        goods2.setPencil(new Pencil());
+        Pencil pencil = goods2.getPencil();
+        // goods1.setApple(new Pencil());  에러 발생
+        // get과 set을 내가 인지해야함
+        // goods1에는 연필 객체를 담을수도 없음.
+        // 모든 상품마다 Goods 클래스가 필요함
+    }
 }
---
+++ Generic/src/GenericClass/Generic_2.java
@@ -0,0 +1,41 @@
+package GenericClass;
+
+// 모든 클래스의 부모인 Object를 이용하면 문제 해결 가능
+
+class Goods {
+    private Object object = new Object();
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
+
+public class Generic_2 {
+    public static void main(String[] args) {
+        Goods goods1 = new Goods();
+        goods1.setObject(new Apple());
+        // getObjetct()는 Object를 리턴하므로 Apple로 다운캐스팅 가능
+        Apple apple = (Apple) goods1.getObject();
+
+        Goods goods2 =new Goods();
+        goods2.setObject(new Pencil());
+        Pencil pencil = (Pencil) goods2.getObject();
+
+        Pencil pencil1 = (Pencil)goods1.getObject();  // 런타임 에러 발생
+//        Apple이 담겨있는 goods1 객체에서 Pencil을 꺼냈지만
+//        컴파일 에러가 발생하지 않는다. 왜?
+//        프로그램 실행시에 에러가 발생됨
+//        Object는 모든 클래스의 부모클래스로서 모든 타입의 객체 저장이 가능한데
+//        자바의 특성상 Object는 약한 타입체크 방식으로만 동작한다
+//        그러므로 컴파일 시점에는 타입이 맞는지 여부를 확인할 수 없으며
+//        런타임 시 실제 타입 기반으로 확인하게 됨
+//        run 누르기전의 에러: 컴파일 에러
+//        run 누르고나서의 에러: 런타임 에러
+
+
+    }
+}
