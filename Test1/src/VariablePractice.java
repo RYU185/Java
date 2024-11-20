24c3930e907cc88d39e1551ec2af15470c0ea6bd
RYU185 <fence365@naver.com>
2024-11-20 16:30:30 +0900

    Create VariablePractice.java

---
+++ Test1/src/VariablePractice.java
@@ -0,0 +1,8 @@
+public class VariablePractice {
+    public static void main(String[] args) {
+        System.out.println("1"+"2");
+        System.out.println('A'+'B');
+        System.out.println('1'+'2');
+        char ch = ' ';
+    }
+}
